40af734a4f9032c07a47dd1359153600ce575f8e
ymizuno <m3i1z4u1n5o@gmail.com>
2025-05-06 12:50:01 +0900

    Read spreadsheet from Google Drive

--- map.js
+++ map.js
@@ -1,29 +1,38 @@
 
 // List of marker
-const markers = [
-    {x: 79, y: "", z: 378, name: '拠点', zoomMin: -1, zoomMax: 3, description: "ここは拠点。"},
-    {x: -400, y: "", z: -300, name: '村', zoomMin: -1, zoomMax: 3, description: "雪山の斜面に面した村。立地最悪。アイアンゴーレムが1体。"},
-    {x: -6, y: "", z: 0, name: 'リスポーン地点', zoomMin: 4, zoomMax: 7, description: "還る場所を無くした魂が辿り着く場所。"},
-    {x: 128, y: -18, z: 553, name: 'スケルトンスポナー', zoomMin: 1, zoomMax: 7, description: "落下式スケさんトラップ。"},
-    {x: -1529, y: "", z: 463, name: '海底遺跡', zoomMin: 1, zoomMax: 7, description: "深いところにあるため未探索"},
-    {x: 134, y: -25, z: 589, name: '試練の間', zoomMin: 1, zoomMax: 7, description: "おちゃが死にかけた。未探索"},
-    {x: -157, y: "", z: -364, name: 'イグルー', zoomMin: 1, zoomMax: 7, description: "村人ゾンビ蘇生中。"},
-    {x: 790, y: "", z: 675, name: '海底遺跡', zoomMin: 1, zoomMax: 7, description: "一部が地上に出ている。シーランタンあり。"},
-    {x: 769, y: "", z: 1471, name: '壊れたポータル', zoomMin: 1, zoomMax: 7, description: "鉄ツルハシがなくて金ブロック未回収。"},
-    {x: -603, y: "", z: 1096, name: '海底遺跡', zoomMin: 1, zoomMax: 7, description: "未探索。"},
-    // {x: -284, y: "", z: 1479, name: '炭鉱', zoomMin: 1, zoomMax: 7, description: "予定地。"},
-    {x: -2480, y: "", z: 800, name: '村', zoomMin: -1, zoomMax: 3, description: "村"},
-    {x: -2590, y: "", z: 1280, name: '村', zoomMin: -1, zoomMax: 3, description: "村"},
-    {x: -2010, y: "", z: 1960, name: '村', zoomMin: -1, zoomMax: 3, description: "村"},
-    {x: -395, y: 6, z: 1382, name: 'アメジストジオード', zoomMin: 1, zoomMax: 7, description: "「ジオード」が一発で変換できなくて予測変換が「時オードリー」。"},
-    {x: -588, y: "", z: 1668, name: '壊れたポータル', zoomMin: 1, zoomMax: 7, description: "チェストの中身回収済み。"},
-    {x: -475, y: -9, z: 1375, name: 'スケルトンスポナー', zoomMin: 1, zoomMax: 7, description: "逃げたので未制圧。"},
-    {x: -307, y: "", z: 248, name: '壊れたポータル', zoomMin: 1, zoomMax: 7, description: "水没。"},
-    {x: -1200, y: "", z: 1006, name: '海底遺跡', zoomMin: 1, zoomMax: 7, description: "未探索。"},
-    {x: -1099, y: "", z: 810, name: '海底遺跡', zoomMin: 1, zoomMax: 7, description: "未探索。"},
-    {x: -1199, y: "", z: 490, name: '海底遺跡', zoomMin: 1, zoomMax: 7, description: "未探索。"},
-    {x: -1499, y: "", z: 506, name: '海底遺跡', zoomMin: 1, zoomMax: 7, description: "未探索。"},
-];
+// const markers = [
+//     {x: 79, y: "", z: 378, name: '拠点', zoomMin: -1, zoomMax: 3, description: "ここは拠点。"},
+//     {x: -400, y: "", z: -300, name: '村', zoomMin: -1, zoomMax: 3, description: "雪山の斜面に面した村。立地最悪。アイアンゴーレムが1体。"},
+//     {x: -6, y: "", z: 0, name: 'リスポーン地点', zoomMin: 4, zoomMax: 7, description: "還る場所を無くした魂が辿り着く場所。"},
+//     {x: 128, y: -18, z: 553, name: 'スケルトンスポナー', zoomMin: 1, zoomMax: 7, description: "落下式スケさんトラップ。"},
+//     {x: -1529, y: "", z: 463, name: '海底遺跡', zoomMin: 1, zoomMax: 7, description: "深いところにあるため未探索"},
+//     {x: 134, y: -25, z: 589, name: '試練の間', zoomMin: 1, zoomMax: 7, description: "おちゃが死にかけた。未探索"},
+//     {x: -157, y: "", z: -364, name: 'イグルー', zoomMin: 1, zoomMax: 7, description: "村人ゾンビ蘇生中。"},
+//     {x: 790, y: "", z: 675, name: '海底遺跡', zoomMin: 1, zoomMax: 7, description: "一部が地上に出ている。シーランタンあり。"},
+//     {x: 769, y: "", z: 1471, name: '壊れたポータル', zoomMin: 1, zoomMax: 7, description: "鉄ツルハシがなくて金ブロック未回収。"},
+//     {x: -603, y: "", z: 1096, name: '海底遺跡', zoomMin: 1, zoomMax: 7, description: "未探索。"},
+//     // {x: -284, y: "", z: 1479, name: '炭鉱', zoomMin: 1, zoomMax: 7, description: "予定地。"},
+//     {x: -2480, y: "", z: 800, name: '村', zoomMin: -1, zoomMax: 3, description: "村"},
+//     {x: -2590, y: "", z: 1280, name: '村', zoomMin: -1, zoomMax: 3, description: "村"},
+//     {x: -2010, y: "", z: 1960, name: '村', zoomMin: -1, zoomMax: 3, description: "村"},
+//     {x: -395, y: 6, z: 1382, name: 'アメジストジオード', zoomMin: 1, zoomMax: 7, description: "「ジオード」が一発で変換できなくて予測変換が「時オードリー」。"},
+//     {x: -588, y: "", z: 1668, name: '壊れたポータル', zoomMin: 1, zoomMax: 7, description: "チェストの中身回収済み。"},
+//     {x: -475, y: -9, z: 1375, name: 'スケルトンスポナー', zoomMin: 1, zoomMax: 7, description: "逃げたので未制圧。"},
+//     {x: -307, y: "", z: 248, name: '壊れたポータル', zoomMin: 1, zoomMax: 7, description: "水没。"},
+//     {x: -1200, y: "", z: 1006, name: '海底遺跡', zoomMin: 1, zoomMax: 7, description: "未探索。"},
+//     {x: -1099, y: "", z: 810, name: '海底遺跡', zoomMin: 1, zoomMax: 7, description: "未探索。"},
+//     {x: -1199, y: "", z: 490, name: '海底遺跡', zoomMin: 1, zoomMax: 7, description: "未探索。"},
+//     {x: -1499, y: "", z: 506, name: '海底遺跡', zoomMin: 1, zoomMax: 7, description: "未探索。"},
+// ];
+
+let markers = [];
+
+const csvUrl = "https://sheets.googleapis.com/v4/spreadsheets/17_VMKtk2IYbEzf0A04EKAy5E3JMLPamxYFRiwlLemQc/values/sheet1?key=AIzaSyD1uf3BzD9GPxQ-_XkKQPhXLu1rH3BwMpc";
+
+// https://docs.google.com/spreadsheets/d/17_VMKtk2IYbEzf0A04EKAy5E3JMLPamxYFRiwlLemQc/edit?usp=sharing
+// AIzaSyD1uf3BzD9GPxQ-_XkKQPhXLu1rH3BwMpc
+// https://sheets.googleapis.com/v4/spreadsheets/17_VMKtk2IYbEzf0A04EKAy5E3JMLPamxYFRiwlLemQc/values/sheet1?key=AIzaSyD1uf3BzD9GPxQ-_XkKQPhXLu1rH3BwMpc
+
 
 // Size of map
 const chunkXMin = -177;
@@ -71,8 +80,54 @@ function updateMarker(vectorSource) {
 }
 
 
-function loadCsv() {
-    // TODO
+async function loadCsv() {
+    try{
+        const res = await fetch(csvUrl);
+        if(!res.ok){
+            throw new Error('HTTP error: ${res.status}');
+        }
+        const data = await res.json();
+
+        const rows = data.values;
+        const headers = rows[0];
+        const entries = rows.slice(1);
+
+        return entries.map(row =>{
+            const obj = {};
+            headers.forEach((key, i) => {
+                obj[key] = i < row.length ? row[i] : "";
+            });
+            obj.x = parseFloat(obj.x);
+            obj.y = parseFloat(obj.y);
+            obj.z = parseFloat(obj.z);
+            obj.minZoom = parseFloat(obj.minZoom);
+            obj.maxZoom = parseFloat(obj.maxZoom);
+            return obj;
+        });
+    } catch (error) {
+        console.error("can not fetch csv");
+        return [{x: 79, y: "", z: 378, name: '拠点', zoomMin: -1, zoomMax: 3, description: "ここは拠点。"}];
+    }
+    // fetch(csvUrl)
+    //     .then(response => response.json())
+    //     .then(data => {
+    //         const rows = data.values;
+    //         const headers = rows[0];
+    //         const entries = rows.slice(1);
+
+    //         markers = entries.map(row =>{
+    //             const obj = {};
+    //             headers.forEach((key, i) => {
+    //                 obj[key] = i < row.length ? row[i] : "";
+    //             });
+    //             obj.x = parseFloat(obj.x);
+    //             obj.y = parseFloat(obj.y);
+    //             obj.z = parseFloat(obj.z);
+    //             obj.minZoom = parseFloat(obj.minZoom);
+    //             obj.maxZoom = parseFloat(obj.maxZoom);
+    //             return obj;
+    //         });
+    //     });
 }
 
 function getElement() {
@@ -83,7 +138,7 @@ function getElement() {
     toggleMarker = document.getElementById("toggle-marker");
 }
 
-function addMainLayer() {
+function addMainLayer(markers) {
 
     var imageExtent = [chunkXMin * 16, (chunkZMax + 1) * (-16), (chunkXMax + 1) * 16, chunkZMin * (-16)];
 
@@ -194,13 +249,8 @@ function addMainLayer() {
 
 }
 
-
-$(function() {
-    loadCsv();
-
+document.addEventListener('DOMContentLoaded', async () => {
+    markers = await loadCsv();
     getElement();
-
-    addMainLayer();
-
-    
+    addMainLayer(markers);
 });
